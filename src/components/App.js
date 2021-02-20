49a4f28fea6425db777ef532023931491c772c06
ashwani436 <sharmagpl786@gmail.com>
2021-02-21 01:16:40 +0530

    save changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
-const App = (props) => {
-  return (
+const App = (props) =>{
+  return(
     <div>
     <Welcome/>
     </div>
